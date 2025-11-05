6a759ebdad687153886039e6b315886cbe219791
oscar1234 <oscarizquierd224@gmail.com>
2025-11-05 19:50:20 +0100

    [ADD] Creation of the routing structure

--- app-book-library/src/app/app.routes.ts
+++ app-book-library/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'Libreria-Salamanca', pathMatch:'full'},
+    {path: 'Libreria-Salamanca', loadComponent: () => import('./pages/book-list-component/book-list-component').then(componente => componente.BookListComponent)},
+    {path: 'Detalle-libro', loadComponent: () => import('./pages/book-detail-component/book-detail-component').then( componente => componente.BookDetailComponent)}
+];
+
